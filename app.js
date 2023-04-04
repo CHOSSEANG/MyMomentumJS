11fa3a76956596eb760a70c4ba9140e33ed185d7
SeongHun_Cho <jng00153@gmail.com>
2023-04-04 22:25:08 +0900

    first exercise sample

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("hi");
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick(){
+    const clickedClass = "clicked";
+   if(h1.className=clickedClass){
+    h1.className="";
+   } else{
+    h1.className=clickedClass
+   }
+}
+
+h1.addEventListener("click",handleTitleClick);
