837ec2cf5d34a41246b5ea723d746191a8f74dd1
SeongHun_Cho <jng00153@gmail.com>
2023-04-10 05:33:26 +0900

    create the delete Todo parts

--- JS/todo.js
+++ JS/todo.js
@@ -5,8 +5,12 @@ const toDoList = document.querySelector("#todo-list");
 function paintTodo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText="X";
+    button.addEventListener("click",deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
@@ -15,6 +19,13 @@ function handleToDoSubmit(event){
     const newTodo = toDoInput.value;
     toDoInput.value="";
     paintTodo(newTodo);
+}
+
+function deleteTodo(event){
+    // 이걸로 button의 부모 element를 확인할 수 있음
+    const li = event.target.parentNode;
+    // 그리고 그냥 해당 element를 remove함
+    li.remove();
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit)
