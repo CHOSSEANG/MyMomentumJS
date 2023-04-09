2e6ec5d84dd94961e7615e7adb0c27e4674b7d0e
SeongHun_Cho <jng00153@gmail.com>
2023-04-10 02:31:16 +0900

    hide the form after enter the value

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting")
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME="hidden";
 
 function onLoginSubmit(tomato){
     tomato.preventDefault();
-    console.log(loginInput.value);
+    const username = loginInput.value;   
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    console.log(username);
+    greeting.innerHTML=`Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit",onLoginSubmit);
